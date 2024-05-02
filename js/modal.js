00e0e924a7cec0585a9cf06bf31b4ebc657ae713
Santana <101057083+hrstan@users.noreply.github.com>
2024-05-01 22:50:23 -0300

    modal

---
+++ js/modal.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() { 
+    const modalExemplo = new bootstrap.Modal('#exemplo-modal'); 
+    setTimeout(function(){ 
+        modalExemplo.show();
+
+    }, 3000)
+})
